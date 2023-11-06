30011f0f28d41ed5e6dfdf7c2340461c05d1308e
mrmarzuh <mrmarzuh@gmail.com>
2023-11-06 09:26:02 +0200

    Created About page

---
+++ web/src/pages/About.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-gray-100 min-h-full">
+    <div class="pt-24">
+      <div class="container mx-auto px-6">
+        <div class="bg-white rounded shadow-lg p-6 md:p-12">
+          <div class="text-center">
+            <h2 class="text-2xl font-semibold mb-6">About 1oT Weather Forecast</h2>
+            <p class="text-gray-700 mb-6">
+              This website provides up-to-date weather forecasts,
+              sourced from the reliable data at
+              <a href="http://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng"
+                 class="text-blue-600 hover:underline" target="_blank">
+                Estonian Weather Service
+              </a>. Our aim is to offer users a convenient interface
+              to access weather information with ease. We are dedicated to providing a
+              service that helps people plan their days better by
+              staying informed about the weather.
+            </p>
+            <p class="text-gray-700">
+              This project as a test assignment for 1oT.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
